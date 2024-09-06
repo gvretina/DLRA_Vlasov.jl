f9abbd4feb44ca362e5d6a126a6731ee9241e67a
Documenter.jl <documenter@juliadocs.github.io>
2024-09-06 16:09:07 +0000

    build based on 297a567

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DLRA_Vlasov","category":"page"},{"location":"#DLRA_Vlasov","page":"Home","title":"DLRA_Vlasov","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DLRA_Vlasov.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DLRA_Vlasov]","category":"page"}]
+}
